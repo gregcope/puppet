f99b213c66aac38f9044eb7ce6b84efd5e94c7a5
gregcope <gregcope@gmail.com>
2014-09-23 21:25:12 +0100

    Added zabbix user to adm group

--- myth/zabbix.pp
+++ myth/zabbix.pp
@@ -186,3 +186,12 @@ file { '/etc/zabbix/zabbix_agentd.d/userparameter_tcpstat.conf':
     require => [ Package [ 'zabbix-agent' ], File [ '/var/lib/zabbix' ] ],
     content => "UserParameter=proc.net.tcp.count.established,/bin/grep -Ec '[0-9A-F]{8}:[0-9A-F]{4} [0-9A-F]{8}:[0-9A-F]{4} 01' /proc/net/tcp\nUserParameter=sockstat.sockets,/bin/cat /proc/net/sockstat|/bin/grep sockets|/usr/bin/cut -d' ' -f 3\nUserParameter=sockstat.tcp.inuse,/bin/cat /proc/net/sockstat|/bin/grep TCP|/usr/bin/cut -d' ' -f 3\nUserParameter=sockstat.tcp.orphan,/bin/cat /proc/net/sockstat|/bin/grep TCP|/usr/bin/cut -d' ' -f 5\nUserParameter=sockstat.tcp.timewait,/bin/cat /proc/net/sockstat|/bin/grep TCP|/usr/bin/cut -d' ' -f 7\nUserParameter=sockstat.tcp.allocated,/bin/cat /proc/net/sockstat|/bin/grep TCP|/usr/bin/cut -d' ' -f 9\nUserParameter=sockstat.tcp.mem,/bin/cat /proc/net/sockstat|/bin/grep TCP|/usr/bin/cut -d' ' -f 11\nUserParameter=sockstat.udp.inuse,/bin/cat /proc/net/sockstat|/bin/grep UDP:|/usr/bin/cut -d' ' -f 3\nUserParameter=sockstat.udp.mem,/bin/cat /proc/net/sockstat|/bin/grep UDP:|/usr/bin/cut -d' ' -f 5",
 }
+
+# add zabbix user to adm group on ubuntu so that they can read log files etc..
+user { "zabbix":
+    ensure => present,
+    require => Package [ 'zabbix-agent' ],
+    notify => Service [ 'zabbix-agent' ],
+    groups => "adm",
+    membership => minimum,
+}
